7d3fdc46984086b7a63dd91585ee65a40827e183
Filippo-Galli <filippo.galli.cr@gmail.com>
2026-02-11 10:16:18 +0100

    Brave flags

--- nixos/programs/brave.nix
+++ nixos/programs/brave.nix
@@ -1,15 +1,22 @@
-{pkgs, ...}:{
-
+{ pkgs, ... }:
+{
   programs.brave = {
     enable = true;
-
     commandLineArgs = [
       "--enable-features=UseOzonePlatform"
       "--ozone-platform=wayland"
       "--password-store=gnome-libsecret"
       "--disable-features=WebRtcAllowInputVolumeAdjustment"
     ];
-    
+    extensions = [ /* optional */ ];
   };
 
+  home.file.".config/etc/brave/policies/managed/brave_prefs.json".text = ''
+    {
+      "BraveRewardsDisabled": true,
+      "BraveWalletDisabled": true,
+      "PaymentMethodsDisabled": true,
+      "BraveVPNDisabled": true
+    }
+  '';
 }
