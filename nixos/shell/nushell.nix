295aef31341b95a75622b74e2269bfa84093446b
Filippo-Galli <filippo.galli.cr@gmail.com>
2026-03-11 14:53:38 +0100

    minor fixes

--- nixos/shell/nushell.nix
+++ nixos/shell/nushell.nix
@@ -10,24 +10,30 @@
     enable = true;
 
     shellAliases = {
-      rebuild = "sudo nixos-rebuild switch --flake .#nixos";
+      test-build = "sudo nixos-rebuild test --flake ~/Documents/Dotfiles/nixos#nixos";
+      rebuild = "sudo nixos-rebuild switch --flake ~/Documents/Dotfiles/nixos#nixos";
       vpn_up = "sudo openconnect --background --protocol=anyconnect --user=figalli vpn-01-dc1.inria.fr";
-      vpn_status = "pgrep -x openconnect | complete | if ($in.exit_code == 0) { 'VPN connected' } else { 'VPN disconnected' }";
       rcat = "cat";
       term = "kitty";
       "..." = "cd ../..";
+      ls= "ls -a";
     };
 
     extraConfig = ''
       def update [] {
-        cd ~/Documents/Dotfiles/nixos
+        pushd ~/Documents/Dotfiles/nixos
         sudo nix flake update
         sudo nixos-rebuild switch --upgrade
+        popd
       }
 
       def vpn_down [] {
         sudo pkill -SIGINT openconnect
         if $env.LAST_EXIT_CODE != 0 { print "No openconnect process found" }
+      }
+
+      def vpn_status [] {
+        if (pgrep -x openconnect | length) > 0 { 'VPN connected' } else { 'VPN disconnected' }
       }
     '';
   };
