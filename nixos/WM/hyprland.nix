6f97949480a63d741e3449ea7750aaa057f52150
Filippo-Galli <filippo.galli.cr@gmail.com>
2026-04-03 15:07:59 +0200

    [hosts] different scaling values host-dependent

--- nixos/WM/hyprland.nix
+++ nixos/WM/hyprland.nix
@@ -3,6 +3,7 @@
   pkgs,
   inputs,
   lib,
+  osConfig,
   ...
 }:
 {
@@ -12,8 +13,21 @@
     xwayland.enable = true;
     extraConfig =
       builtins.replaceStrings
-        [ "/home/filippo/Documents/Dotfiles/nixos/WM/wallpaper/starship.jpg" ]
-        [ "${./wallpaper/starship.jpg}" ]
+        [
+          "/home/filippo/Documents/Dotfiles/nixos/WM/wallpaper/starship.jpg"
+          "$MONITORS_scale = 1.5"
+        ]
+        [
+          "${./wallpaper/starship.jpg}"
+          "$MONITORS_scale = ${
+            if osConfig.networking.hostName == "escanor" then
+              "2.0"
+            else if osConfig.networking.hostName == "gyomei" then
+              "1.5"
+            else
+              "1.5"
+          }"
+        ]
         (builtins.readFile ./config/hypr/hyprland.conf);
   };
 }
