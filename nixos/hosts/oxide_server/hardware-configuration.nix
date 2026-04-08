776042bf2f6f7dab71d9152ee07f393a3b7b8853
Filippo-Galli <filippo.galli.cr@gmail.com>
2026-04-08 11:20:50 +0200

    [oxide_server] hardware added

---
+++ nixos/hosts/oxide_server/hardware-configuration.nix
@@ -0,0 +1,30 @@
+{ lib, modulesPath, ... }:
+
+{
+  imports = [ "${modulesPath}/profiles/qemu-guest.nix" ];
+  services.qemuGuest.enable = true;
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelParams = [ "console=ttyS0,115200n8" ];
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos";
+    fsType = "ext4";
+    autoResize = true;
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/ESP";
+    fsType = "vfat";
+  };
+
+  boot.initrd.availableKernelModules = [
+    "virtio_pci"
+    "virtio_blk"
+    "virtio_scsi"
+    "virtio_net"
+  ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+}
