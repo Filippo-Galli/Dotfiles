6202183d5dfda9a6657210d5b4b43670fda2f48e
Filippo-Galli <filippo.galli.cr@gmail.com>
2026-04-15 16:03:49 +0200

    [gyomei] added screenshot using SUPER+S

--- nixos/WM/hyprland.nix
+++ nixos/WM/hyprland.nix
@@ -16,6 +16,7 @@
         [
           "/home/filippo/Documents/Dotfiles/nixos/WM/wallpaper/starship.jpg"
           "$MONITORS_scale = 1.5"
+          "bind = , XF86Calculator, exec, grim -g \"$(slurp)\""
         ]
         [
           "${./wallpaper/starship.jpg}"
@@ -26,6 +27,12 @@
               "1.6"
             else
               "1.6"
+          }"
+          "${
+            if osConfig.networking.hostName == "gyomei" then
+              "bind = $mainMod, S, exec, grim -g \"$(slurp)\""
+            else
+              "bind = , XF86Calculator, exec, grim -g \"$(slurp)\""
           }"
         ]
         (builtins.readFile ./config/hypr/hyprland.conf);
